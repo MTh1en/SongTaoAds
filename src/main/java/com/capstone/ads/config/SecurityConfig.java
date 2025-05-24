1d3bbf68a96e7fb1ea5b3459d2b5ed57f90f36a5
MTh1en <mt12122003@gmail.com>
2025-05-25 02:21:33 +0700

    fix/open CORS

--- src/main/java/com/capstone/ads/config/SecurityConfig.java
+++ src/main/java/com/capstone/ads/config/SecurityConfig.java
@@ -65,9 +65,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "X-XSRF-TOKEN", "Content-Type"));
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true); // Quan trọng khi dùng cookie
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
