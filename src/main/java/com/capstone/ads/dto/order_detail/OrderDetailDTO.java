a1563d35337eb630381c7a039ea0f88d549f3f74
MTh1en <mt12122003@gmail.com>
2025-07-25 13:37:36 +0700

    REFACTOR/Order detail response

--- src/main/java/com/capstone/ads/dto/order_detail/OrderDetailDTO.java
+++ src/main/java/com/capstone/ads/dto/order_detail/OrderDetailDTO.java
@@ -1,6 +1,8 @@
 package com.capstone.ads.dto.order_detail;
 
 import com.capstone.ads.dto.CoreDTO;
+import com.capstone.ads.dto.custom_design_request.CustomDesignRequestDTO;
+import com.capstone.ads.dto.edited_design.EditedDesignDTO;
 import com.capstone.ads.model.json.CustomerChoiceHistories;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -23,7 +25,7 @@ public class OrderDetailDTO {
     CustomerChoiceHistories customerChoiceHistories;
     LocalDateTime createdAt;
     LocalDateTime updatedAt;
-    CoreDTO editedDesigns;
-    CoreDTO customDesignRequests;
+    EditedDesignDTO editedDesigns;
+    CustomDesignRequestDTO customDesignRequests;
     CoreDTO orders;
 }
