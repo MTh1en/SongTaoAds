9d04e354fc065e9d2edaba37c59dbae9bacfcdad
MTh1en <mt12122003@gmail.com>
2025-08-10 05:34:56 +0700

    FIX/Fix bug production

--- src/main/java/com/capstone/ads/config/SocketIOConfig.java
+++ src/main/java/com/capstone/ads/config/SocketIOConfig.java
@@ -1,6 +1,7 @@
 package com.capstone.ads.config;
 
 import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.Transport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +10,10 @@ public class SocketIOConfig {
     @Bean
     public SocketIOServer socketIOServer() {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        config.setHostname("0.0.0.0");
         config.setPort(8888);
         config.setOrigin("*");
+        config.setTransports(Transport.WEBSOCKET, Transport.POLLING);
         return new SocketIOServer(config);
     }
 }
