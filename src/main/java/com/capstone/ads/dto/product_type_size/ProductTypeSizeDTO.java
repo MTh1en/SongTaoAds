7235a8c2bed70e0389e00a0d2b79a1de31bd703c
MTh1en <mt12122003@gmail.com>
2025-08-11 22:58:43 +0700

    FIX/Product type size response

--- src/main/java/com/capstone/ads/dto/product_type_size/ProductTypeSizeDTO.java
+++ src/main/java/com/capstone/ads/dto/product_type_size/ProductTypeSizeDTO.java
@@ -2,6 +2,7 @@ package com.capstone.ads.dto.product_type_size;
 
 import com.capstone.ads.dto.CoreDTO;
 import com.capstone.ads.dto.size.SizeDTO;
+import com.capstone.ads.model.enums.DimensionType;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -16,6 +17,7 @@ public class ProductTypeSizeDTO {
     String id;
     Float maxValue;
     Float minValue;
+    DimensionType dimensionType;
     LocalDateTime createdAt;
     LocalDateTime updatedAt;
     CoreDTO productTypes;
