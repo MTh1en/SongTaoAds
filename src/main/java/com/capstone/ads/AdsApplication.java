ed67101783fafa96633c9716edf030a1fedc51fe
MTh1en <mt12122003@gmail.com>
2025-09-01 15:22:22 +0700

    FIX/time zone

--- src/main/java/com/capstone/ads/AdsApplication.java
+++ src/main/java/com/capstone/ads/AdsApplication.java
@@ -1,6 +1,7 @@
 package com.capstone.ads;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,14 +9,19 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableFeignClients
 @EnableCaching
 @EnableAsync
 @EnableScheduling
+@Slf4j
 public class AdsApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
